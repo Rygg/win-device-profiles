b9555fdbd0b2a6783623f58f12f2cb7f2ab4920a
Ville Lepistö <19472622+Rygg@users.noreply.github.com>
2023-05-19 02:19:10 +0300

    Add log configuration method.

--- src/SystemTrayApplication/Extensions/HostBuilderExtensions.cs
+++ src/SystemTrayApplication/Extensions/HostBuilderExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Infrastructure.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using TrayApplication.Components.Windows.Forms;
 using TrayApplication.Components.Windows.Forms.HotKeys;
 using TrayApplication.Components.Windows.Forms.TrayIcon;
@@ -10,7 +11,7 @@ namespace TrayApplication.Extensions;
 /// <summary>
 /// Extension methods to support dependency injections.
 /// </summary>
-internal static class ServiceCollectionExtensions
+internal static class HostBuilderExtensions
 {
     /// <summary>
     /// Extension method for adding the required windows application services.
@@ -19,21 +20,24 @@ internal static class ServiceCollectionExtensions
     /// <returns></returns>
     internal static IHostBuilder AddTrayApplicationServices(this IHostBuilder hostBuilder)
     {
-        hostBuilder.ConfigureServices(services =>
-        {
-            services.AddSingleton<IWindowsHotKeyEventSender, KeyboardHotKeyHandle>(); // Add keyboard hot key handle as singleton.
-            services.AddSingleton<TrayIconBuilder>();
-            services.AddSingleton<DeviceProfilesApplicationContext>();
-            services.ConfigureLogging();
-        });
-        return hostBuilder;
+        return hostBuilder
+            .ConfigureLogging()
+            .ConfigureServices(services =>
+            {
+                services.AddSingleton<IWindowsHotKeyEventSender, KeyboardHotKeyHandle>(); // Add keyboard hot key handle as singleton.
+                services.AddSingleton<TrayIconBuilder>();
+                services.AddSingleton<DeviceProfilesApplicationContext>();
+            });
     }
 
     /// <summary>
     /// Configures the logging for the application.
     /// </summary>
-    internal static IServiceCollection ConfigureLogging(this IServiceCollection services)
+    private static IHostBuilder ConfigureLogging(this IHostBuilder builder)
     {
-        return services.AddLogging();
+        return builder.ConfigureLogging((context,options) =>
+        {
+            options.AddConfiguration(context.Configuration);
+        });
     }
 }
